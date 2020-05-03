db957d6583e3a0d572857c2989bae01556e296ef
DEW211 <czuridavid@hotmail.com>
2020-05-03 18:18:24 +0200

    added conditional rendering based on store

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import Albums from './components/Albums';
 import Album from './components/Album';
-import Header from './components/Header'
-import CssBaseline  from "@material-ui/core/CssBaseline"
-function App() {
+import Header from './components/Header';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
-  return (
-    <div>
-    <CssBaseline />
-    <Header />
-    <Album />
-    </div>
+import { connect } from 'react-redux';
 
-  );
+function App(props) {
+	return (
+		<div>
+			<CssBaseline />
+			<Header />
+      {(props.state.isEditing) ? (<Album/>) : (<Albums/>)}
+		</div>
+	);
 }
 
-export default App;
+const mapStateToProps = (state) => {
+	return {
+		state,
+	};
+};
+
+export default connect(mapStateToProps)(App);
