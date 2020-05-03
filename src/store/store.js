a30436b2515ab2680159efb02529b2f4dd5d9d6a
DEW211 <czuridavid@hotmail.com>
2020-05-03 19:12:39 +0200

    added redux dev tools

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,16 @@
 import thunkMiddleware from 'redux-thunk';
 //import { createLogger } from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { editReducer } from '../reducers/editReducer';
 
 //const loggerMiddleware = createLogger();
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(
 	editReducer,
-	applyMiddleware(thunkMiddleware)
+	/* preloadedState, */ composeEnhancers(applyMiddleware(thunkMiddleware))
 );
+/*export const store = createStore(
+	editReducer,
+	applyMiddleware(thunkMiddleware),
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);*/
