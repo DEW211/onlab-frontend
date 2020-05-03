a73a66d152e16c7eef2c06e52e58eb96e999a4a3
DEW211 <czuridavid@hotmail.com>
2020-05-03 18:16:23 +0200

    now reads from redux state

--- src/components/Album.js
+++ src/components/Album.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import {connect} from 'react-redux';
+import { DELETE_PICTURE } from '../actions/ActionTypes';
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -35,9 +36,8 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-export default function Album() {
+function Album(props) {
   const classes = useStyles();
 
   return (
@@ -50,22 +50,17 @@ export default function Album() {
         <Container className={classes.cardGrid} maxWidth="xl">
           {/* End hero unit */}
           <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
+            {props.state.currentAlbum.Pictures.map((card) => (
+              <Grid item key={card.id} xs={12} sm={6} md={4}>
                 <Card className={classes.card}>
                   <CardMedia
                     className={classes.cardMedia}
-                    image="https://picturestoragernalbi.blob.core.windows.net/albums/1.jpg?sv=2019-10-10&ss=bfqt&srt=sco&sp=rwdlacup&se=2020-05-09T19:58:41Z&st=2020-05-03T11:58:41Z&spr=https&sig=0aQ2VlJzfH4ZBhCizuD6HrYCuxLT6A6PyaIvhlKnii0%3D"
+                    image={card.link}
                     title="Image title"
                   />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Title
-                    </Typography>
-                  </CardContent>
                   <CardActions>
-                    <Button size="small" color="primary">
-                      View & Edit
+                    <Button size="small" color="primary" onClick={() => props.dispatch({type: DELETE_PICTURE, id: card.id})}>
+                     DELETE
                     </Button>
                     
                   </CardActions>
@@ -78,3 +73,16 @@ export default function Album() {
     </React.Fragment>
   );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    state
+  }
+}
+
+
+
+export default connect(
+  mapStateToProps,
+  null
+)(Album);
