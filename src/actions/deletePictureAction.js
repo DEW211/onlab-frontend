484f994e8d45a7b667c20905785d72f3ebef8733
DEW211 <czuridavid@hotmail.com>
2020-05-03 23:29:53 +0200

    delete picture action

--- src/actions/deletePictureAction.js
+++ src/actions/deletePictureAction.js
@@ -1,8 +1,13 @@
-import {DELETE_PICTURE} from './ActionTypes';
-
-export function deletePictureAction(picture = []){
-    return {
-        type: DELETE_PICTURE,
-        picture
-    }
+import { fetchAlbum } from './albumActions';
+export function deletePictureAction(pictureId, owner, title) {
+	return function (dispatch) {
+		return fetch(
+			`http://localhost:32777/api/album/${owner}/${title}/${pictureId}`,
+			{ method: 'DELETE', mode: 'cors' }
+		).then((data) => {
+			dispatch(fetchAlbum(owner, title));
+		});
+	};
 }
+
+//delete hívás a végpontra, album újratöltése
